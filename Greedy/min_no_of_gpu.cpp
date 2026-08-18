711e71110d2bab3ab373c234a0f7d7868c517407
Muntajim04 <rahman.saimon@yahoo.com>
2026-08-18 20:07:22 +0600

    fixed errors

--- Greedy/min_no_of_gpu.cpp
+++ Greedy/min_no_of_gpu.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
 
@@ -10,23 +11,24 @@ struct Task
     int finish;
 };
 
-bool cmp(Task a, Task b)
+bool cmp(const Task &a, const Task &b)
 {
     return a.start < b.start;
 }
 
 int getNumOfMinimumGPU(vector<Task> &tasks)
 {
+    if (tasks.empty()) return 0;
 
     sort(tasks.begin(), tasks.end(), cmp);
 
+    // Min-heap to store the finish times of active tasks
     priority_queue<int, vector<int>, greater<int>> pq;
 
     int maxGPU = 0;
 
-    for (Task task : tasks)
+    for (const Task &task : tasks)
     {
-
         while (!pq.empty() && pq.top() <= task.start)
         {
             pq.pop();
@@ -42,12 +44,16 @@ int getNumOfMinimumGPU(vector<Task> &tasks)
 
 int main()
 {
+    
 
     int n;
-    cin >> n;
+    if (!(cin >> n) || n <= 0)
+    {
+        cout << 0 << '\n';
+        return 0;
+    }
 
     vector<Task> tasks(n);
-
     for (int i = 0; i < n; i++)
     {
         cin >> tasks[i].start >> tasks[i].finish;
